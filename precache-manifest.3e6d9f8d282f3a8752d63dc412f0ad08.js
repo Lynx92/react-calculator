852a7d73c666346c853bb61a63c6c9d4a281396f
Gabriel Belmonte <gabelmo92@hotmail.com>
2019-12-17 13:26:19 +0100

    Updates

---
+++ precache-manifest.3e6d9f8d282f3a8752d63dc412f0ad08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71151625eedef47f6c25b51a954e42a0",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "dd396078a28e847dafc9",
+    "url": "/react-calculator/static/css/main.93b07619.chunk.css"
+  },
+  {
+    "revision": "593afbcd5a9011f0bae5",
+    "url": "/react-calculator/static/js/2.85fd933d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-calculator/static/js/2.85fd933d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd396078a28e847dafc9",
+    "url": "/react-calculator/static/js/main.3965c8a5.chunk.js"
+  },
+  {
+    "revision": "16b6cba497c4958a02a9",
+    "url": "/react-calculator/static/js/runtime-main.f8237558.js"
+  }
+]);
